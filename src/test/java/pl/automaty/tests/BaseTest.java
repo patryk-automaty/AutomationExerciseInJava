d0971af936238b263b9e60b47c5187f6428b2ee1
patryk <patkycautomaty@gmail.com>
2025-01-08 12:21:06 +0000

    add BaseTest, DriverFactory and RegisterTest

---
+++ src/test/java/pl/automaty/tests/BaseTest.java
@@ -0,0 +1,21 @@
+package pl.automaty.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import pl.automaty.utils.DriverFactory;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = DriverFactory.getDriver();
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/pl/automaty/tests/RegisterTest.java
@@ -0,0 +1,11 @@
+package pl.automaty.tests;
+
+import org.testng.annotations.Test;
+
+public class RegisterTest {
+    @Test
+    public void RegisterUser() {
+        System.out.println("Mock");
+    }
+}
+
