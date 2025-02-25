34abad0d79d5a3d92170bc1cd9670223316bd194
patryk <patkycautomaty@gmail.com>
2025-02-25 16:40:57 +0000

    upgrade TestCaseTest

--- src/test/java/pl/automaty/tests/TestCaseTest.java
+++ src/test/java/pl/automaty/tests/TestCaseTest.java
@@ -9,20 +9,26 @@ import pl.automaty.pages.TestCasePage;
 
 public class TestCaseTest extends BaseTest {
 
+    // TC 7
     @Test
     public void TestCasePageTest() {
-        // Create instances
+
+        // Define expected value
+        final String expectedTestCaseHeader = "TEST CASES";
+
+        // Create instances for reporting and pages
         HomePage homePage = new HomePage(driver);
         TestCasePage testCasePage = new TestCasePage(driver);
-        ExtentTest test = extentReports.createTest("Verify Subscription in home page");
+        ExtentTest test = extentReports.createTest("Verify Test Cases Page");
 
         // Accept cookies and navigate to test case page
         homePage.consentCookies()
                 .testCase();
-        test.log(Status.PASS, "Accept cookies and navigate to test case page");
+        test.log(Status.PASS, "Accepted cookies and navigated to test case page");
 
         // Verify that the test case header contains the text 'TEST CASES'
-        Assert.assertEquals(testCasePage.getTestCaseHeader(), "TEST CASES");
-        test.log(Status.PASS, "Verify that the test case header contains the text 'TEST CASES'");
+        String actualTestCaseHeaderText = testCasePage.getTestCaseHeader();
+        Assert.assertEquals(actualTestCaseHeaderText, expectedTestCaseHeader);
+        test.log(Status.PASS, "Verified that the test case header contains the text 'TEST CASES'");
     }
 }
