62f1f0ca2f55101c40e4f14e1bcd089ea59f8904
patryk <patkycautomaty@gmail.com>
2025-05-10 12:24:54 +0000

    update DriverFactory to CI

--- src/test/java/pl/automaty/utils/DriverFactory.java
+++ src/test/java/pl/automaty/utils/DriverFactory.java
@@ -3,12 +3,25 @@ package pl.automaty.utils;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 
 public class DriverFactory {
 
     public static WebDriver getDriver() {
         WebDriverManager.chromedriver().setup();
+
+        ChromeOptions options = new ChromeOptions();
+
+        String ci = System.getenv("CI");
+
+        if ("true".equalsIgnoreCase(ci)) {
+            options.addArguments("--headless=new");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            options.addArguments("--disable-gpu");
+        }
+        options.addArguments("--remote-allow-origins=*");
         return new ChromeDriver();
     }
 }
